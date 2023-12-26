9f4d1e8a0b1136e6a06e8a6afd56ab3f28458ed8
Harman Preet Singh <harmanprt5142@gmail.com>
2023-12-26 18:09:21 +0000

    commit

---
+++ mapFunction.js
@@ -0,0 +1,15 @@
+// problem 1 use map function to double the each element of the array arr
+let array = [5,7,8,89,1,7,18,,9,12,52,6]
+// problem 2  squares each element in an array and returns a new array with the squared values.
+
+
+
+// problem 3 Create a function that takes an array of words and a prefix string, and returns a new array where each word is prefixed with the specified prefix.
+const arr = ["harman","biny","chandigarh","delhi"]
+function addPrefix(arr, prefix) {
+    // your code here
+    // you can use prefix of your choice
+  }
+  
+// problem 4   Using the map function return the length of each element in array arr
+
